d45e99d5f65a3beba57984335f566a0071c5d6ac
joeypercia <jperc004@ucr.edu>
2021-05-01 22:00:43 -0700

    Fixed select_not

--- select.hpp
+++ select.hpp
@@ -4,7 +4,6 @@
 #include <string>
 #include <cstring>
 
-
 class Select
 {
 public:
@@ -12,6 +11,9 @@ public:
 
     // Return true if the specified row should be selected.
     virtual bool select(const Spreadsheet* sheet, int row) const = 0;
+    virtual std::string getColumn() = 0;
+    virtual std::string getString() = 0;
+    virtual Spreadsheet* getSpreadsheet() = 0;
 };
 
 // A common type of criterion for selection is to perform a comparison based on
@@ -40,15 +42,14 @@ public:
     virtual bool select(const std::string& s) const = 0;
 };
 
-
-
-
 // WORK IN PROGRESS!!
 class Select_Contains : public Select
 {
 protected:
-    int column;
+    std::string column;
+    int columnindex;
     std::string _userstring;
+    Spreadsheet* _sheet;
 
 public:
     bool select(const std::string& s) const
@@ -62,32 +63,44 @@ public:
     }
 
     bool select(const Spreadsheet* sheet, int row) const {
-        return select(sheet->cell_data(row, this->column));
+        return select(sheet->cell_data(row, this->columnindex));
     }
 
     Select_Contains(Spreadsheet* sheet, std::string column, std::string userstring)
     {
         //sheet->set_selection(nullptr); // clear selections, probably not needed here.
         this->_userstring = userstring;
-        this->column = sheet->get_column_by_name(column);
+        this->column = column;
+        this->columnindex = sheet->get_column_by_name(column);
+        this->_sheet = sheet;
         for (int i = 0; i < sheet->getnumRows(); ++i) // start at 1st row after column names, end once final roww has been reached. Not sure if gets correct size.
         {
-            select(sheet->cell_data(i, this->column)); // query each row to see if it should be selected
+            select(sheet->cell_data(i, this->columnindex)); // query each row to see if it should be selected
             // ??? do more stuff ???
         }
 
     }
 
+    std::string getString() {
+        return this->_userstring;
+    }
+    std::string getColumn() {
+        return this->column;
+    }
+    Spreadsheet* getSpreadsheet() {
+        return this->_sheet;
+    }
 
 };
-
-
 // WORK IN PROGRESS!!
 class Select_Not : public Select
 {
 protected:
-    int column;
+    int columnindex;
+    std::string _column;
     std::string _userstring;
+    Spreadsheet* _sheet;
+    Select* _selectptr;
 
 public:
     bool select(const std::string& s) const
@@ -101,25 +114,75 @@ public:
     }
 
     bool select(const Spreadsheet* sheet, int row) const {
-        return select(sheet->cell_data(row, this->column));
+        return select(sheet->cell_data(row, this->columnindex));
     }
 
-    Select_Not(Spreadsheet* sheet, std::string column, std::string userstring)
-    {
-        //sheet->set_selection(nullptr); // clear selections, probably not needed here.
-        this->_userstring = userstring;
-        this->column = sheet->get_column_by_name(column);
-        for (int i = 0; i < sheet->getnumRows(); ++i) // start at 1st row after column names, end once final roww has been reached. Not sure if gets correct size.
-        {
-            select(sheet->cell_data(i, this->column)); // query each row to see if it should be selected
-            // ??? do more stuff ???
+    //void Select_Notp(Spreadsheet* sheet, std::string column, std::string userstring)
+    //{
+    //    //sheet->set_selection(nullptr); // clear selections, probably not needed here.
+    //    this->_userstring = userstring;
+    //    this->columnindex = sheet->get_column_by_name(column);
+    //    for (int i = 0; i < sheet->getnumRows(); ++i) // start at 1st row after column names, end once final roww has been reached. Not sure if gets correct size.
+    //    {
+    //        select(sheet->cell_data(i, this->columnindex)); // query each row to see if it should be selected
+    //    }
+    //}
+
+    Select_Not(Select* selectptr) {
+        
+        this->_selectptr = selectptr;
+        this->_userstring = selectptr->getString();
+        this->_sheet = selectptr->getSpreadsheet();
+        this->_column = selectptr->getColumn();
+        this->columnindex = _sheet->get_column_by_name(selectptr->getColumn());
+        
+        for (int i = 0; i < _sheet->getnumRows(); ++i) {
+            select(_sheet->cell_data(i, this->columnindex));
         }
-
     }
-
-
+    std::string getString() {
+        return this->_userstring;
+    }
+    std::string getColumn() {
+        return this->_column;
+    }
+    Spreadsheet* getSpreadsheet() {
+        return this->_sheet;
+    }
 };
 
-
-
+//class Select_And : public Select
+//{
+//protected:
+//    Select* _leftop;
+//    Select* _rightop;
+//
+//public:
+//    bool select(const std::string& s) const
+//    {
+//        if ((_leftop->select()) == true) && ((_rightop->select()) == true) { // if both select conditions return true, return true.
+//            return true;
+//        }
+//        else {
+//            return false;
+//        }
+//    }
+//
+//    bool select(const Spreadsheet* sheet, int row) const {
+//        return select(sheet->cell_data(row, this->column));
+//    }
+//
+//    Select_And(Select* leftop, Select* rightop)
+//    {
+//        _leftop = leftop;
+//        _rightop = rightop;
+//
+//        for (int i = 0; i < sheet->getnumRows(); ++i)
+//        {
+//            select(sheet->cell_data(i, this->column));
+//        }
+//
+//    }
+//
+//};
 #endif //__SELECT_HPP__
